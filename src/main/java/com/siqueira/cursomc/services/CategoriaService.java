2bb32573fa509cec41c51f0c1fbddee451afe993
Tiago Vieira Siqueira <tiagov@badesc.gov.br>
2020-04-30 16:20:39 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/siqueira/cursomc/services/CategoriaService.java
+++ src/main/java/com/siqueira/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.siqueira.cursomc.domain.Categoria;
 import com.siqueira.cursomc.repositories.CategoriaRepository;
+import com.siqueira.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -17,7 +18,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	} 
 
 }
