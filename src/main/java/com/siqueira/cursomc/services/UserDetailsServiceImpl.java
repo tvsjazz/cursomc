ea5eaa6b447a5bf0d4dd236905e5b699b80866c9
Tiago Siqueira <tvsjazz@gmail.com>
2020-05-21 11:10:12 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/siqueira/cursomc/services/UserDetailsServiceImpl.java
+++ src/main/java/com/siqueira/cursomc/services/UserDetailsServiceImpl.java
@@ -4,11 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.siqueira.cursomc.domain.Cliente;
 import com.siqueira.cursomc.repositories.ClienteRepository;
 import com.siqueira.cursomc.security.UserSS;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 	
 	@Autowired
