c1285f8302229c110eaa68b9e3c35cc9227f3442
Tiago Siqueira <tvsjazz@gmail.com>
2022-02-16 08:30:39 -0300

    feat(Categoria): Criar repository e service para categoria

--- src/main/java/com/tiago/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/tiago/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,11 @@
 package com.tiago.cursomc.resources;
 
+import com.tiago.cursomc.domain.Categoria;
+import com.tiago.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
-    @GetMapping
-    public String listar() {
-        return "REST está funcionando";
+    @Autowired
+    private CategoriaService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/tiago/cursomc/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.tiago.cursomc.services;
+
+import com.tiago.cursomc.domain.Categoria;
+import com.tiago.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+    }
+}
